b033eb80e9006e4e720e5e87fd432df39d5058ec
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-03-26 00:31:34 +0530

    Update stu-one.js

--- js/stu-one.js
+++ js/stu-one.js
@@ -150,8 +150,18 @@ else{
     $('#chooseavtrbrd').toggle('fast');
   });
   $('#clsavtrbrd').click(function(){
-
     $('#avtrbrdstsec').toggle('fast');
+    var bd = document.body;
+if(bd.style.overflow !="hidden"){
+  bd.style.overflow = 'hidden';
+  bd.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+else{
+  bd.style.overflow ="auto";
+  bd.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
   });
   function addavtrtobrd(label){
     var list=document.getElementsByClassName("avtrpictemp");
