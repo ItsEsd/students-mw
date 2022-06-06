4e590350fb2e827beda249aaf05d6555cf684a21
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-06-07 00:40:12 +0530

    Update wallcook.js

--- js/wallcook.js
+++ js/wallcook.js
@@ -6,9 +6,9 @@ var ewfSetCookie = function(exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toUTCString();
-    document.cookie = "mwallcstu=true; expires=" + expires + ";path=/";
-    document.cookie = "mwallpswedus="+btoa(psmed)+"; expires=" + expires + ";path=/";
-    document.cookie = "mwallpswedud="+btoa(pswed)+"; expires=" + expires + ";path=/";
+    document.cookie = "mwallcstu=true; expires=" + expires + ";path=/;domain=mastrowall.com";
+    document.cookie = "mwallpswedus="+btoa(psmed)+"; expires=" + expires + ";path=/;domain=mastrowall.com";
+    document.cookie = "mwallpswedud="+btoa(pswed)+"; expires=" + expires + ";path=/;domain=mastrowall.com";
   };
   
   function getCookie(cname) {
