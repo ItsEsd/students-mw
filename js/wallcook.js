a7d425bd85c1b17b6ef9cb5d5db511e44acff084
ItsEsd <81977626+ItsEsd@users.noreply.github.com>
2022-06-08 13:54:28 +0530

    Update wallcook.js

--- js/wallcook.js
+++ js/wallcook.js
@@ -1,37 +1,30 @@
-
 var ewfSetCookie = function(exdays) {
-    
     var psmed = $("#email").val();
     var pswed = $("#pcodeStu").val();
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toUTCString();
-    document.cookie = "mwallcstu=true; expires=" + expires + ";path=/;domain=mastrowall.com";
-    document.cookie = "mwallpswedus="+btoa(psmed)+"; expires=" + expires + ";path=/;domain=mastrowall.com";
-    document.cookie = "mwallpswedud="+btoa(pswed)+"; expires=" + expires + ";path=/;domain=mastrowall.com";
+    document.cookie = "mwallcstu=true; expires=" + expires + ";path=/;domain=127.0.0.1";
+    document.cookie = "mwallpswstus="+btoa(psmed)+"; expires=" + expires + ";path=/;domain=127.0.0.1";
+    document.cookie = "mwallpswstud="+btoa(pswed)+"; expires=" + expires + ";path=/;domain=127.0.0.1";
   };
-  
   function getCookie(cname) {
     var name = cname + "="; 
     var decodedCookie = decodeURIComponent(document.cookie); 
     var ca = decodedCookie.split(';'); 
-    for (var i = 0; i < ca.length; i++) {
-       var c = ca[i]; 
-    
-      } 
 for(var p=0;p<ca.length;p++){
-var cookstr = ca[p].split('mwallcstu=')[1];
-if(cookstr=="true"){
-  var paem = atob(ca[p+1].split('mwallpswedus=')[1]);
-  var pacd = atob(ca[p+2].split('mwallpswedud=')[1]);
+var cookstrem = ca[p].split('mwallpswstus='); 
+var cookstrkd = ca[p].split('mwallpswstud='); 
+if(cookstrem[0] == 0){
+  var paem = window.atob(cookstrem[1]);
   document.getElementById('email').value= paem;
+}
+else if(cookstrkd[0]== 0){
+  var pacd = window.atob(cookstrkd[1]);
   document.getElementById('pcodeStu').value= pacd;
   inwallStu();
-}
-}} 
-  
-    $(document).ready(function(){getCookie() })  ;
-  
+}}}
+  $(document).ready(function(){getCookie() })  ;
   function deleteAllCookies() {
    var cookies = document.cookie.split(";");
        for (var i = 0; i < cookies.length; i++) {
