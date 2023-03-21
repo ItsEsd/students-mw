77980165ee16d3ba75ac932e14940783cb56faa5
S Das <d.soubhik@hotmail.com>
2023-03-21 18:20:28 +0530

    Update wallcook.js

--- js/wallcook.js
+++ js/wallcook.js
@@ -25,16 +25,6 @@ else if(cookstrkd[0]== 0){
   inwallStu();
 }}}
   $(document).ready(function(){getCookie() })  ;
-  function deleteAllCookies() {
-   var cookies = document.cookie.split(";");
-       for (var i = 0; i < cookies.length; i++) {
-         var cookie = cookies[i];
-           var eqPos = cookie.indexOf("=");
-         var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-         document.cookie = name + "=true;"+"expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/";
-               }
-              setTimeout(function(){location.reload();},2000);
-       }     
        function deleteAllCookies() {
         var cookies = document.cookie.split(";");
         for (var i = 0; i < cookies.length; i++) {
