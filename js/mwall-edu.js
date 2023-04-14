35d08f0609b498a4e4ea11093d90896ee25d6f94
S Das <d.soubhik@hotmail.com>
2023-04-14 22:47:25 +0530

    Update mwall-edu.js

--- js/mwall-edu.js
+++ js/mwall-edu.js
@@ -539,11 +539,11 @@ var date = new Date();
 var tois = date.toISOString();
 var flcaldate = tois.substring(0, 10);
   var calendar = new FullCalendar.Calendar(calendarEl, {aspectRatio: 1,
-    initialView: 'listWeek',
+    initialView: 'dayGridMonth',
     headerToolbar: {
       left: 'prev,next',
       center: 'title',
-      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+      right: 'dayGridYear,dayGridMonth,timeGridWeek,timeGridDay'
     },
     initialDate: flcaldate,
     navLinks: true, 
