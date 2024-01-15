9b26ad0a17bdb1ad0a2e11a7cbc17e7bf4a19d18
soubhikdas <d.soubhik@hotmail.com>
2024-01-15 16:05:49 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,45 @@
 // Installing service worker
-const CACHE_NAME  = 'MWALLStudent';
+const CACHE_NAME = "MWALLStudent";
 
 /* Add relative URL of all the static content you want to store in
  * cache storage (this will help us use our app offline)*/
-let resourcesToCache = ["../vendor/js/","../vendor/style/"];
+let resourcesToCache = [
+  "/vendor/js/",
+  "/vendor/style/",
+  "/images/",
+  "/style-2.css",
+  "/style.css",
+];
 
-self.addEventListener("install", e=>{
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache =>{
-            return cache.addAll(resourcesToCache);
-        })
-    );
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(resourcesToCache);
+    })
+  );
 });
 
 // Cache and return requests
-self.addEventListener("fetch", e=>{
-    e.respondWith(
-        caches.match(e.request).then(response=>{
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
 
 // Update a service worker
-const cacheWhitelist = ['MWALLStudent'];
-self.addEventListener('activate', event => {
-    event.waitUntil(
-      caches.keys().then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cacheName => {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    );
-  });
+const cacheWhitelist = ["MWALLStudent"];
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
